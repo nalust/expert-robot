23a1a239766497ce1ab862f016445d31cfcb08bb
Leonardo de Almeida <leodealmeida9@gmail.com>
2022-10-07 22:00:45 -0300

    fix exercises 8 and 11

--- while_for/ex08_duvida.py
+++ while_for/ex08_duvida.py
@@ -1,10 +1,19 @@
-a = float(input("Informe o primeiro número: "))
-b = float(input("Informe o segundo número: "))
-c = float(input("Informe o terceiro número: "))
-d = float(input("Informe o quarto número: "))
-e = float(input("Informe o quinto número: "))
+# a = float(input("Informe o primeiro número: "))
+# b = float(input("Informe o segundo número: "))
+# c = float(input("Informe o terceiro número: "))
+# d = float(input("Informe o quarto número: "))
+# e = float(input("Informe o quinto número: "))
 
-soma = a + b + c + d + e
-media = (a + b + c + d + e) / 5
+# soma = a + b + c + d + e
+# media = (a + b + c + d + e) / 5
 
+# print("A soma dos números é", soma, ". A média dos números é", media, ".")
+
+soma = 0
+for numero in range(5):
+    print("Informe o número", numero + 1)
+    valor = float(input())
+    soma = soma + valor
+
+media = soma/5
 print("A soma dos números é", soma, ". A média dos números é", media, ".")
--- while_for/ex11_duvida.py
+++ while_for/ex11_duvida.py
@@ -1,12 +1,18 @@
-from re import I
+# a = int(input("Informe um número: "))
+# b = int(input("Informe outro número: "))
 
+# for i in range (a + 1, b):
+#     print(i)
+
+# for i in range (a, b + 1):
+#     print(i)
+# print("A soma dos números é", i + i)
 
 a = int(input("Informe um número: "))
 b = int(input("Informe outro número: "))
 
-for i in range (a + 1, b):
-    print(i)
-
+soma = 0
 for i in range (a, b + 1):
-    print(i)
-print("A soma dos números é", i + i)
+    # pegar o valor do numero atual (i) e somar na variavel soma
+
+print("A soma dos números é", soma)
