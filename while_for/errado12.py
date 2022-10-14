0ca8a94ce03c4c7929f376d7c40b49d0e047273d
Ana <analuisasousasantos@gmail.com>
2022-10-13 21:52:46 -0300

    exercício errado 12

---
+++ while_for/errado12.py
@@ -0,0 +1,9 @@
+numero = int(input("Tabuada do número: "))
+
+print("Tabuada de", numero)
+
+tabuada = 0
+
+while numero <= 10:
+    print(numero, "X", tabuada, "=", numero * tabuada)
+    tabuada = tabuada + 1
