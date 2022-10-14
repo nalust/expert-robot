4a270c400d784c1cdb2763fa5a5b8a627fc1e87d
Ana <analuisasousasantos@gmail.com>
2022-10-13 21:20:16 -0300

    exercícios estruturas de repetição

---
+++ while_for/ex12.py
@@ -0,0 +1,7 @@
+numero = int(input("Tabuada do número: "))
+
+print("Tabuada de", numero)
+
+for tabuada in range(10):
+     print(numero, "x", tabuada + 1, "=", (tabuada + 1) * numero)
+     
---
+++ while_for/ex13_dica.py
@@ -0,0 +1,4 @@
+base = int(input("Informe a base: "))
+expoente = int(input("Informe o expoente: "))
+
+
