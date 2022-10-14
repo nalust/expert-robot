0e3b3cb07687c250ff8a7195d6379dc9bfa1fad6
Leonardo de Almeida <leodealmeida9@gmail.com>
2022-10-13 22:24:13 -0300

    solve exp

--- while_for/ex13_dica.py
+++ while_for/ex13_dica.py
@@ -1,4 +1,18 @@
 base = int(input("Informe a base: "))
 expoente = int(input("Informe o expoente: "))
 
+resultado = 1
+variavel_auxiliar = False
+
+if (expoente < 0):
+    expoente = -1 * expoente
+    variavel_auxiliar = True
+
+for contador in range(expoente):
+    resultado = resultado * base
+
+if (variavel_auxiliar == True):
+    print("Resultado:", 1/resultado)
+else:
+    print("Resultado:", resultado)
 
